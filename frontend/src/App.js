cf22735673375951249cd37d1b9106a1dd0f1454
Sarvesh Bodakhe <sarveshbodakhe77@gmail.com>
2023-10-26 19:39:23 +0530

    Video-03-Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+       <a href="/">amazona</a>
+      </header>
+      <main>List products</main>
+    </div>
+  );
+}
+
+export default App;
